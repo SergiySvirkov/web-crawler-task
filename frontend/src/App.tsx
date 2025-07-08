b31ce4bb9c631a8c7f42b8c0a6fb721f2c6f9f3d
SergiySvirkov <124861145+SergiySvirkov@users.noreply.github.com>
2025-07-08 09:50:38 +0300

    Create App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Sykell Web Crawler</h1>
+        <p>Analysis Dashboard</p>
+      </header>
+      {/* The main content of the dashboard will be rendered here later */}
+    </div>
+  );
+}
+
+export default App;
